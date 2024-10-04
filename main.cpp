1a8e1b30558e9e01a8c9ec12ee281f06f49c8808
Eldry <eldryjesu@gmail.com>
2024-10-04 10:02:29 -0400

    agregar escenario

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <SFML/Graphics.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Graphics.hpp>
+#include<iostream>
+#include <vector>
+
+#include <settings.hpp>
+#include <char.hpp>
+#include <animation.hpp>
+#include <stateStack.hpp>
+#include <stage.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window{ sf::VideoMode{Settings::WINDOW_WIDTH,Settings::WINDOW_HEIGHT} , "Demo", sf::Style::Resize | sf::Style::Close | sf::Style::Titlebar };//Ventana
+    
+    sf::RenderTexture render_texture{};
+    render_texture.create(Settings::VIRTUAL_WIDTH, Settings::VIRTUAL_HEIGHT);
+
+    //Escala de sprite
+    sf::Vector2f scale_factors
+    {
+        float(Settings::WINDOW_WIDTH) / float(Settings::VIRTUAL_WIDTH),
+        float(Settings::WINDOW_HEIGHT) / float(Settings::VIRTUAL_HEIGHT)
+    };
+    sf::Sprite render_sprite{ render_texture.getTexture() };//Asignacion de la textura a un sprite
+    render_sprite.setScale(scale_factors);
+
+
+
+    StateStack allStages{};
+
+    sf::Texture background;
+
+    background.loadFromFile("assets/textures/mainStage.png");
+    std::shared_ptr<Stage> mainStage = std::make_shared<Stage>(Settings::VIRTUAL_WIDTH, Settings::VIRTUAL_HEIGHT, sf::Sprite{ background });
+    allStages.pushStage(mainStage);
 
     while (window.isOpen())
     {
@@ -15,8 +45,11 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        render_texture.clear(sf::Color::Black);
+        render_texture.draw(allStages.getCurrentStage()->get_sprite());
+        render_texture.display();
+
+        window.draw(render_sprite);
         window.display();
     }
 
