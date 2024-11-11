190f98b696c3204b9ca0f95b913b321cefb52803
Trysdan <jdanieldp99@gmail.com>
2024-11-11 10:45:35 -0400

    textura para las casass

--- Jobs.hpp
+++ Jobs.hpp
@@ -29,9 +29,10 @@ public:
     void removeVillagers(JobTypes job);
     void unlockJob(JobTypes job);
 
-    void processJob(Resources &resources, JobTypes job, ResourceTypes inputResource, int inputAmount, ResourceTypes outputResource, int outputAmount) const;
-    void collectResources(Resources &resources, JobTypes _jobs);
+    void processJob(Resources& resources, JobTypes job, ResourceTypes inputResource, int inputAmount, ResourceTypes outputResource, int outputAmount) const;
+    void collectResources(Resources& resources, JobTypes _jobs);
     void collectResourcesAutomatically(Resources& resources);
+
 private:
     std::vector<std::tuple<std::string, int>> jobs;
 };
